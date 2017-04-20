8056d9ec8dd5be14370aa58d043f5d0d25cac111
jhonGit <jhocalderoNN@gmail.com>
2017-04-20 11:38:55 -0600

    prueva para gity jenkins

---
+++ JavaAppSCI/src/model/Producto.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author jcaldeon
+ */
+public class Producto {
+    
+    private int precio;
+    private int codigo;
+    String descripcion;
+    
+    public Producto(){}
+    
+    public Producto(int price,int code,String description){
+    
+        price = precio;
+        code = codigo;
+        description=descripcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
